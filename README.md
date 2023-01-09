a65a229b0e9c979c9a9821f778622dce2314fa90
Ankit Kumar <akg95469@gamil.com>
2023-01-09 22:08:57 +0530

    React project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# LearnReact
+It is repo comprise of all type of react project level from Beginner, Medium and hard.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
+  );
+}
+
+export default App;
